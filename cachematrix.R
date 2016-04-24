bcaeabcb5a96772f4a0bef4115781ebd00659c00
ulfandpete <mail@ulfpetersen.com>
2016-04-25 00:07:34 +0200

    Solved programming assignment 2
    Adjusted the cachematrix.R file to solve the problem. Created the
    cachevector.R file as a reference.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Proposed solution to: Assignment: Programming Assignment 2: Lexical Scoping
+## Function that caches the Inverse of a Matrix
 
-## Write a short comment describing this function
+## This function creates the matrix for which the inverse should be cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(mean) m <<- mean
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function cache-solves the inverse of a matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    if (nrow(data) == ncol(data)) {
+        m <- solve(data, ...)
+    } else {
+        message("not a square matrix. cannot inverse")        
+    }
+    x$setinverse(m)
+    m
 }
